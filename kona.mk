dab251ce5e22489edccf58bcc05c542cdaeeab54
itsurboimasarou <medicspam6@gmail.com>
2022-09-10 18:06:35 +0700

    Revert "sm8250-common: Nuke prebuilt xiaomi.kona USB 1.2 HALs [2/2]"
    This reverts commit f9350eb8ed1d8b47c3eebbd4948f7c1fdbef2eca.

--- kona.mk
+++ kona.mk
@@ -446,6 +446,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.xiaomi
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.2-service-xiaomi_kona
+
 # Vendor service manager
 PRODUCT_PACKAGES += \
     vndservicemanager
